9048aefe00f708dfd7b60db914d270c41e9fd376
Achyutmanas Tiwari <ammt501@gmail.com>
2023-04-15 17:03:43 +0530

    login register data storing completed

--- server/app.js
+++ server/app.js
@@ -7,11 +7,11 @@ dotenv.config({ path: "./config.env" });
 
 require("./database/connection");
 
-const port = process.env.PORT;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(require("./router/auth"));
+
+const port = process.env.PORT;
 
 app.get("/portfolio", (req, res) => {
   res.send("Welcome to portfolio");
@@ -22,14 +22,6 @@ app.get("/transaction", (req, res) => {
 });
 
 app.get("/learn", (req, res) => {
-  res.send("happy learning");
-});
-
-app.get("/login", (req, res) => {
-  res.send("happy learning");
-});
-
-app.get("/register", (req, res) => {
   res.send("happy learning");
 });
 
