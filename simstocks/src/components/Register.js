751e58d6050151c25b898529cd0cbd2dbd0409be
Achyutmanas Tiwari <ammt501@gmail.com>
2023-04-16 14:29:19 +0530

    form created

--- simstocks/src/components/Register.js
+++ simstocks/src/components/Register.js
@@ -9,7 +9,73 @@ const Register = () => {
       <section>
         <div className="signup">
           <div className="container mt-5">
-            <div className="signup-content"></div>
+            <div className="signup-content">
+              <div className="signup-form">
+                <h2 className="form-title">Registeration</h2>
+                <form className="register-form" id="register-form">
+                  <div className="form-group">
+                    <label htmlFor="name">
+                      <i className="zmdi zmdi-account material-icons-name"></i>
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      autoComplete="off"
+                      placeholder="Your Name"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="name">
+                      <i className="zmdi zmdi-account material-icons-name"></i>
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      autoComplete="off"
+                      placeholder="Your Name"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="name">
+                      <i className="zmdi zmdi-account material-icons-name"></i>
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      autoComplete="off"
+                      placeholder="Your Name"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="name">
+                      <i className="zmdi zmdi-account material-icons-name"></i>
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      autoComplete="off"
+                      placeholder="Your Name"
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="name">
+                      <i className="zmdi zmdi-account material-icons-name"></i>
+                    </label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      autoComplete="off"
+                      placeholder="Your Name"
+                    />
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
         </div>
       </section>
